c6debf7fd8a5ebe8f1f4bcde0c6e3bb27f6d675d
KenMarasigan <kpmarasigan@carmensbest.com.ph>
2025-03-18 10:51:18 +0800

    Revert "Merge branch 'main' of https://github.com/JamesLawas123/techsource"
    This reverts commit 1c9a57ddb9bfb12edbd8d1505d465e0eb662ae8a, reversing
    changes made to 04f1c931b62805d7854651740b865bc78a95642f.

--- projectmanagement/threadPage.php
+++ projectmanagement/threadPage.php
@@ -110,8 +110,6 @@ if (mysqli_num_rows($task_result) > 0) {
 		<![endif]-->
 	</head>
 
-        
-
 	<body class="no-skin">
 		<div id="navbar" class="navbar navbar-default          ace-save-state">
 			<div class="navbar-container ace-save-state" id="navbar-container">
@@ -506,7 +504,7 @@ if (mysqli_num_rows($task_result) > 0) {
 							</li>
 
 							<li>
-								<a href="Projectmanagement.php">Task management</a>
+								<a href="#">More Pages</a>
 							</li>
 							<li class="active">Thread Page</li>
 						</ul><!-- /.breadcrumb -->
@@ -846,14 +844,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 Save
             </button>
         </div> -->
-
-		<div style="margin-bottom: 15px; text-align: right; margin-right: 80%;">
-			<a href="projectmanagement.php" class="btn btn-primary btn-sm">
-				<i class="ace-icon fa fa-arrow-left"></i>
-				Back
-			</a>
-   		</div>
-</div>
+    </div>
 
 	
 
